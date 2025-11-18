e87dc2f76062e1885b31a8d345939e33806fe616
odwyem14 <odwyem14@tcd.ie>
2025-11-18 21:46:51 +0000

    consistency for each driver in finishing position

--- src/position_metrics.py
+++ src/position_metrics.py
@@ -12,9 +12,13 @@ positions = df['Position']
 print(f"This is the total standard deviation of finishing positions : {positions.std()}")
 
 
-driver_std = df.groupby("FullName")["Position"].std().reset_index()
+driver_range = df.groupby("FullName")["Position"].agg(
+    lambda x: x.max() - x.min()
+)
+driver_std = df.groupby("FullName")["Position"].std()
 driver_result_mean = df.groupby("FullName")["Position"].mean().reset_index()
 driver_start_mean = df.groupby("FullName")["GridPosition"].mean().reset_index()
+
 
 print("These are the standard deviations in a driver's finishing position \n")
 print(f"{driver_std} \n")
@@ -24,3 +28,18 @@ print(f"{driver_start_mean} \n")
 
 print("These are the mean finishing positions of the drivers \n")
 print(f"{driver_result_mean} \n")
+
+print("These are the range of each driver's finishing position \n")
+print(f"{driver_range} \n")
+
+#checking consistency using standard deviation and range
+driver_names = df["FullName"].unique()
+for name in driver_names:
+    dr_range = driver_range[name]
+    std = driver_std[name]
+    if std/dr_range > 0.35:
+        print(f"{name} is inconsistent in race result")
+    elif std/dr_range <= 0.35 and std/dr_range >= 0.20:
+        print(f"{name} is moderately consistent in race result")
+    elif std/dr_range < 0.2:
+        print(f"{name} is very consistent in race result")
